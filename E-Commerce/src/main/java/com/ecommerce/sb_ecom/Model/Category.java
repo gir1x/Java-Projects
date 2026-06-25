5586d35931686a506bd7576540018e69a376c2ce
g1rix <mrmistake2004@gmail.com>
2026-06-25 19:20:39 +0530

    "used H2 database"

--- E-Commerce/src/main/java/com/ecommerce/sb_ecom/Model/Category.java
+++ E-Commerce/src/main/java/com/ecommerce/sb_ecom/Model/Category.java
@@ -1,9 +1,16 @@
 package com.ecommerce.sb_ecom.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
+@Entity(name = "categories")
 @Data
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
     private Long categoryId;
     private String categoryName;
 }
